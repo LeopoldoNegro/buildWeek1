0d5d469b1d0d4e8e1563ac408484ddca0d603ac8
SimonePeraz <simoneperaz177@gmail.com>
2022-04-21 15:41:30 +0200

    day3Anim2Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,22 @@ function reveal(){
     if(revealtop < windowheight - revealpoint){
       reveals[i].classList.add('active');
     }
-    // else{
-    //   reveals[i].classList.remove('active');
+  }
+}
 
-    // }
+
+window.addEventListener('scroll', reveal2);
+
+function reveal2(){
+  var reveals = document.querySelectorAll('.reveal2');
+
+  for(var i = 0; i < reveals.length; i++){
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
   }
 }
